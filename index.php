d406eec50bd323f37e8b5cfa519b5863048cb88a
Nikhil Gupta <nikhilgupta123459@gmail.com>
2019-10-12 17:34:03 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,380 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Coderault</title>
+  <link rel="stylesheet" type="text/css" href="css/hpsheet.css">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.Cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script type="text/javascript"  src="js/caroufredsel.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+  <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat+Subrayada&display=swap" rel="stylesheet">
+</head>
+<body>
+  <header>
+      <a href="insidelogo/insidelogo.php" class="ab"><img src="cr1.png"></a> 
+      <a href="insidelogo/insidelogo.php" class="cd"><img src="cr3.png"></a> 
+    <div class="container">
+      <div class="rightmenu">
+        <a href="" class="logo"><img src=""></a>
+        <nav>
+          <ul>
+      
+            <li><a href="login/login.php" class="ni">LOGIN</a></li>
+            <li><a href="development/development.php" class="ni">DEVELOPMENT</a></li>
+            <li><a href="contact/contactus.php" class="ni">CONTACT US</a></li>
+          </ul>
+          
+        </nav>
+        
+      </div>
+      
+    </div>
+    
+  </header>
+<section class="slider">
+  <ul class="slider-carousel" id="slider-carousel">
+    <li class="img1">
+      <h2>CODE<span>RAULT</span></h2>
+      <p>IT'S YOUR TIME...! CHANGE IT</p>
+      <a href="" class="btngs">Get Started</a>
+    </li>
+    <li class="img2">
+      <h2>DE<span>SIGN</span> & DEVELOP<span>MENT</span></h2>
+      <p>We have proud on our web designs</p>
+      <a href="" class="btngs">Get Started</a>
+    </li>
+  </ul>
+  <script type="text/javascript" src="js/main.js"></script>
+</section>
+<section class="brief-area white" id="brief">
+<div class="container">
+  <div class="row">
+    <div class="col-sm-12 text-center">
+      <h2>DESCENT WORK</h2>
+      <div class="sub-heading">
+      <p>We are young minds with a dynamo here<br>
+      to provide easy and effective web designs.
+    </p>
+      </div>
+    </div>
+</div>
+<div class="row">
+	<div class="col-sm-4">
+		<div class="intro-block">
+			<span class="intro-icon"><i class="fa fa-paint-brush"></i></span>
+			<h3>Unique design</h3>
+			<p>We are young minds with a dynamo here<br>
+      to provide easy and effective web designs.</p>
+		</div>
+		
+	</div>
+	<div class="col-sm-4">
+		<div class="intro-block">
+			<span class="intro-icon"><i class="fa fa-superpowers"></i></span>
+			<h3>Quality Assurance</h3>
+			<p>We are young minds with a dynamo here<br>
+      to provide easy and effective web designs.</p>
+		</div>
+		
+	</div>
+	<div class="col-sm-4">
+		<div class="intro-block">
+			<span class="intro-icon"><i class="fa fa-database"></i></span>
+			<h3>Database</h3>
+			<p>We are young minds with a dynamo here<br>
+      to provide easy and effective web designs.</p>
+		</div>
+		
+	</div>
+</div>
+</div>  
+</section>
+<section class="feature-area" id="feature">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12 text-center">
+				<div class="title">
+						 <h2>DESCENT WORK</h2>
+     						 <div class="sub-heading">
+     						 <p>We are young minds with a dynamo here<br>
+      						to provide easy and effective web designs.
+    						</p>
+					</div>
+					
+				</div>
+				
+			</div>
+			
+		</div>
+		<div class="row">
+			<div class="col-sm-12 col-lg-8 text-center">
+				<div class="feature-block">
+					<div class="single-feature feature-circle">
+						<div class="circle"></div>
+						<span class="feature-icon"><i class="fa fa-exchange"></i></span>
+						<h3>custom scrollbars</h3>
+						<p>
+							We are young minds with a dynamo here<br>
+      						to provide easy and effective web designs.
+						</p>
+					</div>
+					<div class="single-feature">
+						<span class="feature-icon"><i class="fa fa-star"></i></span>
+						<h3>robust</h3>
+						<p>
+							We are young minds with a dynamo here<br>
+      						to provide easy and effective web designs.
+
+						</p>
+					</div>
+					<div class="single-feature">
+						<span class="feature-icon"><i class="fa fa-star"></i></span>
+						<h3>data</h3>
+						<p>
+							We are young minds with a dynamo here<br>
+      						to provide easy and effective web designs.
+
+						</p>
+					</div>
+					<div class="single-feature">
+						<span class="feature-icon"><i class="fa fa-star"></i></span>
+						<h3>best development</h3>
+						<p>
+							We are young minds with a dynamo here<br>
+      						to provide easy and effective web designs.
+
+						</p>
+					</div>
+				</div>
+				
+			</div>
+			<div class="col-sm-4 col-lg-4">
+				<img src="https://www.gitinfosys.com/images/gitinfosys-mobile-website-design.png">
+				
+			</div>
+			<div >
+				
+			</div>
+			
+		</div>
+	</div>
+	
+</section>
+
+
+<section class="more-feature-area" id="more-feature">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12 text-center">
+				<div class="title">
+						 <h2>website feature</h2>
+     						 <div class="sub-heading">
+     						 <p>We are young minds with a dynamo here<br>
+      						to provide easy and effective web designs.
+    						</p>
+					</div>
+					</div>
+				</div>
+				</div>
+				<div class="row">
+				<div class="col-sm-12 col-md-7">
+					 <img src="https://www.gitinfosys.com/images/gitinfosys-mobile-website-design.png">
+					 </div>
+					 <div class="col-sm-12 col-md-5">
+
+				<div class="feature-list">
+					<ul>
+						<li>
+							<div class="feature-icon">
+								<i class="fa fa-arrows-alt"></i>
+							</div>
+							<div class="feature-details">
+								<h3>RESPONSIVE</h3>
+								<p>We are young minds with a dynamo here<br>
+      						to provide easy and effective web designs.									
+								</p>
+								
+							</div>
+						</li>
+						<li>
+							<div class="feature-icon">
+								<i class="fa fa-cog"></i>
+							</div>
+							<div class="feature-details">
+								<h3>DYNAMIC</h3>
+								<p>
+									We are young minds with a dynamo here<br>
+      						to provide easy and effective web designs.
+								</p>
+								
+							</div>
+						</li>
+						<li>
+							<div class="feature-icon">
+								<i class="fa fa-mobile"></i>
+							</div>
+							<div class="feature-details">
+								<h3>AI CONFIGURED</h3>
+								<p>
+									We are young minds with a dynamo here<br>
+      						to provide easy and effective web designs.
+								</p>
+								
+							</div>
+						</li>
+						<li>
+							<div class="feature-icon">
+								<i class="fa fa-undo"></i>
+							</div>
+							<div class="feature-details">
+								<h3>USER FRIENDLY</h3>
+								<p>
+									We are young minds with a dynamo here<br>
+      						to provide easy and effective web designs.
+								</p>
+								
+							</div>
+						</li>
+					</ul>
+					
+				</div>
+				</div> 
+					 
+				</div>
+			</div>
+	
+</section>
+
+<section class="team-area" id="team">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12 text-center">
+				<h2>OUR TEAM</h2>
+				<div class="sub-heading">
+					<p>
+					our team works for you and for your<br>
+					 needs without any hasitation. 
+					</p>
+				
+				</div>
+			
+			</div>
+		 
+		</div>
+	<div class="row">
+		<div class="col-sm-12 col-md-3">
+			<div class="team-man">
+				<img src="nick.jpg">
+			</div>
+			<div class="team-description">
+				<div class="team-title">
+					<h3>Nikhil Gupta</h3>
+					<span>Founder</span>
+				</div>
+				<p>
+					Nikhil Gupta is a founding partner at coderault.
+					he assumes the role of designer and backend codes.
+					presently, persuing Btech 3rd year in computer 
+					science engineering.
+				</p>
+				 <div class="team-social-network">
+				 	<a href=""><i class="fa fa-facebook"></i></a>
+				 	<a href=""><i class="fa fa-twitter"></i></a>
+				 	<a href=""><i class="fa fa-linkedin"></i></a>
+				 </div>
+			</div>
+		</div>	
+			<div class="col-sm-12 col-md-3">
+			<div class="team-man">
+				<img src="abhi.png">
+			</div>
+			<div class="team-description">
+				<div class="team-title">
+					<h3>Darshana Pillai</h3>
+					<span>project manager</span>
+				</div>
+				<p>
+					Darshana Pillai is also the member of UI design at coderault
+					she assumes the roles of front end codes and designer. presently persuing btech 3rd year in cs engineering.
+				</p>
+				 <div class="team-social-network">
+				 	<a href=""><i class="fa fa-facebook"></i></a>
+				 	<a href=""><i class="fa fa-twitter"></i></a>
+				 	<a href=""><i class="fa fa-linkedin"></i></a>
+				 </div>
+			</div>
+		</div>
+			<div class="col-sm-12 col-md-3">
+			<div class="team-man">
+				<img src="harsh.jpg">
+			</div>
+			<div class="team-description">
+				<div class="team-title">
+					<h3>Harsh morey</h3>
+					<span>project manager</span>
+				</div>
+				<p>
+					Harsh Morey is head of UI design.
+				</p>
+				 <div class="team-social-network">
+				 	<a href=""><i class="fa fa-facebook"></i></a>
+				 	<a href=""><i class="fa fa-twitter"></i></a>
+				 	<a href=""><i class="fa fa-linkedin"></i></a>
+				 </div>
+			</div>
+		</div>
+			<div class="col-sm-12 col-md-3">
+			<div class="team-man">
+				<img src="">
+			</div>
+			<div class="team-description">
+				<div class="team-title">
+					<h3>Kirti Barman</h3>
+					<span>project manager</span>
+				</div>
+				<p>
+					Kirti have massive techniques of solving problems.
+				</p>
+				 <div class="team-social-network">
+				 	<a href=""><i class="fa fa-facebook"></i></a>
+				 	<a href=""><i class="fa fa-twitter"></i></a>
+				 	<a href=""><i class="fa fa-linkedin"></i></a>
+				 </div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+	 
+</section>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
